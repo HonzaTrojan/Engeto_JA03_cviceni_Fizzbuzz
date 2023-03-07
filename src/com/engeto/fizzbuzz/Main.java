5d3a42d2f1f302ec0ee2593da716924fc296fe2b
Jan Trojan <jan.trojan01@gmail.com>
2023-03-07 21:24:02 +0100

    Final version

--- src/com/engeto/fizzbuzz/Main.java
+++ src/com/engeto/fizzbuzz/Main.java
@@ -1,5 +1,22 @@
+package com.engeto.fizzbuzz;
+
 public class Main {
+    /** FIZZBUZZ
+     * Vypisuj čísla od 1 do 100. V případě, že je dané číslo dělitelné třemi,
+     * tak napiš místo něj text Fizz. Pokud je číslo dělitelné pěti, tak místo
+     * čísla vypiš Buzz. A pokud je dělitelní třemi a pěti zároveň, tak vypiš FizzBuzz.
+     */
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        for (int i = 1; i <= 100; i++){
+            if (i % 3 == 0 && i % 5 == 0) System.out.println("FizzBuzz");
+            else if (i % 3 == 0) System.out.println("Fizz");
+            else if (i % 5 == 0) System.out.println("Buzz");
+            else System.out.println(i);
+
+        }
+
     }
 }
